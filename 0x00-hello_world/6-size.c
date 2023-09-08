73c25c4c93dfe8ddef5fd7542c28584781aed23e
israelwalden <waldenisraelk@gmail.com>
2023-09-07 21:32:14 -0700

    task 5

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -A program that prints the size of various types
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char:"sizeof(char);
+printf("Size of an int:"sizeof(int);
+printf9
+}
