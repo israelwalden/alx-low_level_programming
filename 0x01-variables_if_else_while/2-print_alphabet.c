76be4094c764e7323f547915f70027185aed8d8b
israelwalden <waldenisraelk@gmail.com>
2023-09-08 14:53:01 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -print last digit of n
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
