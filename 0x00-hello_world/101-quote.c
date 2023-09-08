c3f7f887ca5ce7b18357ac4d4305adf1ee9757bd
israelwalden <waldenisraelk@gmail.com>
2023-09-07 22:58:08 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<uniistd.h>
+#include<unistd.h>
 /**
  * main - print wihout printf anf puts
  * Return: (1)Success
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-0-19\n", 59);
+write(2, "and that piece of art is useful\n" - Dora Karpar, "2015-10-19\n", 59);
 return (1);
 }
