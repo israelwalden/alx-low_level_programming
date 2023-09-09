615795b225dc805b7cbb81d0103c8bfb3bf20819
israelwalden <waldenisraelk@gmail.com>
2023-09-08 23:15:58 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,12 +11,13 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 
 	{
-	if (letter != 'e' && letter != 'q')
-	putchar(letter);
+		if (letter != 'e' && letter != 'q')
+		putchar(letter);
 	}
 
 	putchar('\n');
 
 	return (0);
 }
+
 	
