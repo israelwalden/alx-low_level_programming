595b4a2df372df8d18fb0a65d6b352d7b4ba3f02
israelwalden <waldenisraelk@gmail.com>
2023-09-11 12:53:50 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 char c;
 int i;
 long l;
-long int li;
+long long int li;
 float fl;
 printf("Size of a char:%lu byte(s)\n", sizeof(c));
 printf("Size of an int:%lu byte(s)\n", sizeof(i));
