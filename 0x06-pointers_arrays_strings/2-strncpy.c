4e572cd1961264b7ea5b9ff8160d9ec770de126d
israelwalden <waldenisraelk@gmail.com>
2023-09-22 08:30:09 +0000

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,7 +17,11 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];/*append char in second string to first string*/
 		i++;/*increase counter*/
 	}
-	dest[i] = '\0';
+	while(i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
 }
