fee937e803b413ae522f2790642b44c902b3e841
israelwalden <waldenisraelk@gmail.com>
2023-10-03 19:59:41 +0000

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,8 +3,7 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: string to be converted 
- *
+ * @s: string to be converted
  * Return: the integer converted from the string
  */
 int _atoi(char *s)
