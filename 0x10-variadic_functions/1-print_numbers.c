637d891bc193387bc6809ae314058e0685baf701
israelwalden <waldenisraelk@gmail.com>
2023-10-13 17:48:28 +0000

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,21 +10,19 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args;   /* Declare a va_list object to handle variable arguments */
-    unsigned int i;
+	va_list ptr; /*Declare a va_list object to handle the varible n*/
+	unsigned int i;/*Declare an unsigned int i*/
 
-    va_start(args, n); /* Initialize 'args' to point to the first variable argument */
+	va_start(ptr, n);/*va function to point ptr to the first value of n*/
 
-    for (i = 0; i < n; i++) /* Loop 'n' times to process each argument */
-    {
-        int num = va_arg(args, int); /* Get the next integer argument from 'args' */
-        printf("%d", num); /* Print the integer */
+for (i = 0; i < n ; i++)/*loops n time through argumnets*/
+	{
+	int n = va_args(ptr, int);/*va function to get next integar argument*/
 
-        if (i < n - 1 && separator != NULL) /* If not the last number and separator is not NULL */
-            printf("%s", separator); /* Print the separator */
-    }
-
-    va_end(args); /* Clean up resources associated with 'args' */
-    printf("\n"); /* Print a newline character */
+	printf("%d", n);/*print the interger*/
+	if (i < n - 1 && seperator != NULL)/*if not last number and seperat not NULL*/
+		printf("%s", seperator);/*Print the separator*/
+	}
+va_end(n);/*realse n resources*/
+printf("\n";)/*print newline*/
 }
-
