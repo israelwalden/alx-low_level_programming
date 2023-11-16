66bbb6faa8e403e1386cc5340963c2f77cd04202
israelwalden <waldenisraelk@gmail.com>
2023-11-16 03:59:49 +0000

    compar task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include " main.h"
+
+/**
+ * wildcmp -comoares two stings 
+ * @s1: string 1
+ * @s2: string 2
+ * Rerturn: returns 1 if string is iedntical else 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	
+}
