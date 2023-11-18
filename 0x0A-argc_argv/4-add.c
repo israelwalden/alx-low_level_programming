4ed6c2f8d6c6ea2c15140dd1f1af02ecad87449f
israelwalden <waldenisraelk@gmail.com>
2023-11-18 01:31:45 +0000

    add task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,23 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - suma of all interger argumants
  * @argc: number of arguments
  * @argv: char pointer to arguments
- * Return: 0
+ * Return: 1 if error else 0
  */
+
+int _digits(char *s);
 
 int main(int argc, char *argv[])
 {
-	int sum;
+	int sum, i;
 
-	while (argc--)/*not zero*/
+	i = 1;
+
+	while (i < argc)/*not zero*/
 	{
-		sum = sum  + atoi(*argv++);
+		if (_digits(argv[i]))
+			sum = sum + atoi(argv[i]);
+		else
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		i++;
 	}
 	printf("%d\n", sum);
-
-return (0);
-
+	return (0);
 }
+
+
+/**
+ * _digits -checks if numbers
+* @s :pointer to stirng
+ * Return: returns 0 if digits else 1
+ */
+int _digits(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (!((s[i] >= 48) && (s[i] <= 57)))
+			return (0);
+	}
+
+	return (1);
+}
+
