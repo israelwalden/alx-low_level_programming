6d56879e6f835819228a575328512983b2f807d2
israelwalden <waldenisraelk@gmail.com>
2023-11-24 01:54:03 +0000

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "main.h"
 
+
+char *fill_mem(char *s, char fill, unsigned int size);
 /**
  *_calloc- allocate memory for an array using
  *@nmemb:number of array elements
@@ -12,22 +13,32 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
-	char *ptr;
+	char *p;
 
-	/*check if nmemb and size are non-zero*/
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * size);/*initialize ptr */
-
-	if (ptr == NULL)/*if pointer fails*/
+	p = malloc(nmemb * size);
+	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-	{
-		ptr[i] = '0';
-	}
 
-	return (ptr);
+	fill_mem(p, 0, nmemb * size);
 
+	return (p);
+}
+
+/**
+ * fill_mem- fill memory with char
+ * @s: pointer to string
+ * @b: char to fill
+ * @n:integer number of char
+ * Return: pointer
+ */
+char *fill_mem(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
