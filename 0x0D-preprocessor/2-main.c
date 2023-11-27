61cfe5d2a04a4a651b196a5d82ac06b0a7a23939
israelwalden <waldenisraelk@gmail.com>
2023-11-27 19:20:01 +0000

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,4 @@
-#include <unistd.h>
-
-int _putchar(char c);
+#include <stdio.h>
 
 /**
  * main - program that prints the name of file
@@ -9,27 +7,6 @@ int _putchar(char c);
 
 int main(void)
 {
-	int i;
-	char *file_name = __FILE__;
-
-	for (i = 0; file_name[i] != '\0'; i++)
-	{
-		_putchar(file_name[i]);
-
-	}
-	_putchar('\n');
+	printf("%s\n", __FILE__);
 	return (0);
-
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
