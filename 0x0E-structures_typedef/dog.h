7a6f342b176bef82609c3040b376214c6b573ad4
israelwalden <waldenisraelk@gmail.com>
2023-11-28 05:27:25 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for dog
+ * @name: string name of dog
+ * @age: float age of dog
+ * @owner: string owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
