0198cd34372847544856af2773ddf2848a6ae8c1
israelwalden <waldenisraelk@gmail.com>
2023-12-11 20:45:52 +0000

    fixed lists.h syntax

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,6 @@
 #ifndef LISTS_H
-#define LIST_H
-
+#define LISTS_H
 #include <stdio.h>
-
-
 
 /**
  * struct listint_s - singly linked list
@@ -11,7 +8,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
