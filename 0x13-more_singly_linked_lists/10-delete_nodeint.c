a354468bf7e0f0fa96e90a61f0ba73493414bfd0
israelwalden <waldenisraelk@gmail.com>
2023-12-12 12:07:48 +0000

    task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - delete node at index
+ * @head: double pointer to head
+ * @index: index to delete
+ * Return: 1 if success, -1 if failure
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 1;
+	listint_t *ptr, *prev;
+
+	ptr  = *head;
+	if (!head ||!*head)
+		return (-1);
+	else
+	{
+		if (index == 0)
+		{
+			*head = ptr->next;
+			free(ptr);
+			return (1);
+		}
+
+
+		if (index > 0)
+		{
+			while (ptr)
+			{
+				if (i == index)
+				{
+					prev = ptr->next;
+					ptr->next = prev->next;
+					free(prev);
+					return (1);
+				}
+				i++;
+				ptr = ptr->next;
+			}
+		}
+
+	} 
+	
+	return (-1);
+}
