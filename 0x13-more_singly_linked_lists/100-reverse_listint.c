5be8c3183da4ae64b4074f424eff7384c300f7b4
israelwalden <waldenisraelk@gmail.com>
2023-12-12 21:12:47 +0000

    task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * revers_listint - reverses the list
+ * @head: pointer to *head
+ * Return: returns pointer to listint_t
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL;
+
+	if (head)
+	{
+		while (*head)
+		{
+			next = *head;
+			*head = (*head)->next;
+			next->next = prev;
+			prev = next;
+		}
+
+		*head = prev;
+		return (*head);
+	}
+
+	return (NULL);
+}
