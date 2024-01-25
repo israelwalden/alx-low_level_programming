346b5b34c9eeb06cdfcde694c92f753cc0dd1782
israelwalden <waldenisraelk@gmail.com>
2024-01-25 10:45:52 +0000

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints the all data in list
+ * @h: head pointer
+ * Return: Returns size_t number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes;
+	const dlistint_t *cur;
+
+	cur = h;
+	nodes = 0;
+
+	while (cur)
+	{
+		printf("%d\n", cur->n);
+		cur = cur->next;
+		nodes++;
+	}
+	return (nodes);
+}
